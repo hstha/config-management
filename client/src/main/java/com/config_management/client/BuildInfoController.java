6878c7f08a4671f526b9037fdece15a88e4750cf
Heriz Shrestha <19641023+hstha@users.noreply.github.com>
2026-08-22 17:49:04 -0400

    - Configured config server and client. - Config server loads configuration from public GitHub - To view configuration of config server go to api `client/<profile>` for server - Configured config client to use to config server configuration - After linking we can use configuration which is in GitHub loaded by `server` in `client` - Since `client` has `dev` profile active dev configuration is loaded if exists from `server`

---
+++ client/src/main/java/com/config_management/client/BuildInfoController.java
@@ -0,0 +1,22 @@
+package com.config_management.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuildInfoController {
+  @Value("${build.id:default}")
+  private String buildId;
+
+  @Value("${build.version:default}")
+  private String buildVersion;
+
+  @Value("${build.name:default}")
+  private String buildName;
+
+  @GetMapping("/build-info")
+  public String getBuildInfo() {
+    return "Build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+  }
+}
--- server/src/main/java/com/config_management/server/ServerApplication.java
+++ server/src/main/java/com/config_management/server/ServerApplication.java
@@ -2,8 +2,15 @@ package com.config_management.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+
+/*
+	It enables the features to be a config server which enable the application to listen to all
+	client configuration
+*/
+@EnableConfigServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
