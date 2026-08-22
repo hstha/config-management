1e7f70d8250c493a3d1e6efd1a58cab71f1c8e26
Heriz Shrestha <19641023+hstha@users.noreply.github.com>
2026-08-22 19:53:01 -0400

    Added a feature to reflect updated application properties on refreshing the application without restarting the application
    - Installed actuator in both `server` and `client`
    - Added `@RefreshScope` in the place where we want updated property value
    - To get the updated value you need to hit
    exposed `/actuator/refresh` api on the client side

--- client/src/main/java/com/config_management/client/BuildInfoController.java
+++ client/src/main/java/com/config_management/client/BuildInfoController.java
@@ -1,10 +1,12 @@
 package com.config_management.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class BuildInfoController {
   @Value("${build.id:default}")
   private String buildId;
